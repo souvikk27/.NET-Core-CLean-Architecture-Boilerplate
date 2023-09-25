884810b61c70d04e2217e48b98063b76301e6ee0
Souvik Kundu <souvik.kundu@pitangent.com>
2023-09-25 12:40:32 +0530

    Added exception and error models

---
+++ Ecommerce.Domain/Exceptions/NotFoundApiException.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Exceptions
+{
+    public class NotFoundApiException : Exception
+    {
+        private const string NotFoundMessage = "The requested resource was not found.";
+
+        public NotFoundApiException() : base(NotFoundMessage)
+        {
+        }
+
+        public NotFoundApiException(string resourceName) : base($"{NotFoundMessage} ({resourceName})")
+        {
+        }
+
+        public NotFoundApiException(string resourceName, Exception innerException) : base($"{NotFoundMessage} ({resourceName})", innerException)
+        {
+        }
+    }
+}
