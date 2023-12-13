5341ebaea8c1378be2d15fada47538c11ac90db2
Souvik Kundu <souvik.kundu@pitangent.com>
2023-12-13 18:30:49 +0530

    Working on Authorization Endpoint

--- Ecommerce.Presentation/Controller/OpenIdController.cs
+++ Ecommerce.Presentation/Controller/OpenIdController.cs
@@ -1,6 +1,9 @@
-﻿using Ecommerce.LoggerService;
-using Ecommerce.OpenAPI.Auth;
+﻿using Ecommerce.OpenAPI.Auth;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using System.Collections.Immutable;
+using System.Net;
 using static OpenIddict.Abstractions.OpenIddictConstants;
 using IAuthenticationService = Ecommerce.OpenAPI.Auth.Abstraction.IAuthenticationService;
 
@@ -11,16 +14,136 @@ namespace Ecommerce.Presentation.Controller;
 
 public class OpenIdController: ControllerBase
 {
+    private readonly IOpenIddictApplicationManager _applicationManager;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IOpenIddictAuthorizationManager _authorizationManager;
     private readonly IAuthenticationService _authService;
+    private readonly IOpenIddictScopeManager _scopeManager;
 
-    public OpenIdController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IAuthenticationService authService)
+    public OpenIdController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IAuthenticationService authService, IOpenIddictApplicationManager applicationManager,
+    IOpenIddictAuthorizationManager authorizationManager, IOpenIddictScopeManager scopeManager)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _authService = authService;
+        _applicationManager = applicationManager;
+        _applicationManager = applicationManager;
+        _scopeManager = scopeManager;
     }
+
+    [HttpGet("~/connect/authorize")]
+    [HttpPost("~/connect/authorize")]
+    [IgnoreAntiforgeryToken]
+    public async Task<IActionResult> Authorize()
+    {
+        var request = HttpContext.GetOpenIddictServerRequest() ??
+        throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+        var result = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        if (!result.Succeeded)
+        {
+            return Challenge(
+                authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                properties: new AuthenticationProperties
+                {
+                    RedirectUri = Request.PathBase + Request.Path + QueryString.Create(
+                        Request.HasFormContentType ? Request.Form.ToList() : Request.Query.ToList())
+                });
+        }
+
+        var user = await _userManager.GetUserAsync(result.Principal) ??
+            throw new InvalidOperationException("The user details cannot be retrieved.");
+
+        var application = await _applicationManager.FindByClientIdAsync(request.ClientId) ??
+            throw new InvalidOperationException("Details concerning the calling client application cannot be found.");
+
+        var authorization = _authorizationManager.FindAsync(subject: await _userManager.GetUserIdAsync(user),
+                                                            client: await _applicationManager.GetIdAsync(application),
+                                                            status: Statuses.Valid,
+                                                            type: AuthorizationTypes.Permanent,
+                                                            scopes: request.GetScopes()) ??
+                    throw new InvalidOperationException("Details concerning the authorization cannot be found.");
+
+        switch (await _applicationManager.GetConsentTypeAsync(application))
+        {
+            case ConsentTypes.External when authorization == null:
+                return Forbid(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.ConsentRequired,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
+                            "The logged in user is not allowed to access this client application."
+                    }));
+
+
+            case ConsentTypes.Implicit:
+            case ConsentTypes.External when authorization != null:
+            case ConsentTypes.Explicit when authorization != null && !request.HasPrompt(Prompts.Consent):
+                // Create the claims-based identity that will be used by OpenIddict to generate tokens.
+                var identity = new ClaimsIdentity(
+                    authenticationType: TokenValidationParameters.DefaultAuthenticationType,
+                    nameType: Claims.Name,
+                    roleType: Claims.Role);
+
+                // Add the claims that will be persisted in the tokens.
+                identity.SetClaim(Claims.Subject, await _userManager.GetUserIdAsync(user))
+                        .SetClaim(Claims.Email, await _userManager.GetEmailAsync(user))
+                        .SetClaim(Claims.Name, await _userManager.GetUserNameAsync(user))
+                        .SetClaims(Claims.Role, (await _userManager.GetRolesAsync(user)).ToImmutableArray());
+
+                // Note: in this sample, the granted scopes match the requested scope
+                // but you may want to allow the user to uncheck specific scopes.
+                // For that, simply restrict the list of scopes before calling SetScopes.
+                identity.SetScopes(request.GetScopes());
+                identity.SetResources((IEnumerable<string>?)_scopeManager.ListResourcesAsync(identity.GetScopes()));
+
+                var auth = authorization.LastOrDefault();
+                auth ??= _authorizationManager.CreateAsync(
+                    identity: identity,
+                    subject: await _userManager.GetUserIdAsync(user),
+                    client: await _applicationManager.GetIdAsync(application),
+                    type: AuthorizationTypes.Permanent,
+                    scopes: identity.GetScopes());
+
+                identity.SetAuthorizationId(await _authorizationManager.GetIdAsync(authorization));
+                identity.SetDestinations(OpenIDAuthService.GetDestinations);
+
+                return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+            case ConsentTypes.Explicit when request.HasPrompt(Prompts.None):
+            case ConsentTypes.Systematic when request.HasPrompt(Prompts.None):
+                return Forbid(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.ConsentRequired,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
+                            "Interactive user consent is required."
+                    }));
+
+            // In every other case, render the consent form.
+            default:
+                return Ok(new AuthorizeDto
+                {
+                    ApplicationName = await _applicationManager.GetLocalizedDisplayNameAsync(application),
+                    Scope = request.Scope
+                });
+        }
+
+        var principal = await _signInManager.CreateUserPrincipalAsync(user);
+        principal.SetScopes(request.GetScopes());
+
+        return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+    }
+
+
+
+
+
 
     [HttpPost("~/connect/token"), IgnoreAntiforgeryToken, Produces("application/json")]
     public async Task<IActionResult> Exchange()
